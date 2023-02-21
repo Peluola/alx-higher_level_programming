296e70af7fd10da0ac26c11ca6dcb94ca93912c2
peluola <marriegold362@gmail.com>
2023-02-21 15:02:22 -0800

    Update

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 12-student.py
+
 """Defines a class Student."""
 
 
@@ -27,4 +27,4 @@ class Student:
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.
+        return self.__dict__
